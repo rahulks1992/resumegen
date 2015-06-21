5c66bf6631284b72b1b74f56eeaf0a998111eb74
Shafeeq Ur Rahman <shafeeq.rahman01@gmail.com>
2015-06-22 00:11:09 +0530

    Cloning with new scope

--- js/script.js
+++ js/script.js
@@ -8,10 +8,48 @@ angular.module('Resumegen', [])
 		}
 	};
 	$scope.addModule = function(whichmodule){
-		var selectedmodule = angular.element( document.querySelector( ('#'+ whichmodule) ) );
-		var newform = angular.element(selectedmodule.children()[2]).clone();
-		angular.element( document.querySelector( ('#center') )).append(newform);
-		var newtemplate = angular.element(selectedmodule.children()[1]).clone();
-		angular.element( document.querySelector( ('#right') )).append(newtemplate);
+		var randomnumber = 10, newmodel = Math.floor(Math.random()*1000000);
+		var form = $('#'+ whichmodule).find('.moduleform').clone(true, true);
+		form.find('input').each(function(i,v){
+			var modelis = $(v).attr('ng-model'),
+			newmodelis = 'defaults' + newmodel,
+			finalmodel = modelis.replace('defaults', newmodelis) ;
+			$(v).attr('ng-model', finalmodel)
+		})
+		form.appendTo($('#center'));
+		var moduletemplate = $('#'+ whichmodule).find('.moduletemplate').clone(true, true);
+		moduletemplate.find('div').each(function(i,v){
+			var modelis = $(v).html(),
+			newmodelis = 'defaults' + newmodel,
+			finalmodel = modelis.replace('defaults', newmodelis) ;
+			$(v).html(finalmodel)
+		})
+		moduletemplate.appendTo($('#right'))
 	}
+	var config = {
+		url : '/api/authenticate',
+		method: 'GET',
+		params: {
+		}
+	},
+	successCallback = function(d) {
+		if(d.data) {
+			sessionStorage.setItem('loggedIn', true);
+			sessionStorage.setItem('userId', $scope.inputId);
+			// $location.path('/home.html#/list');
+			window.location.href = 'home.html#/list';
+		}
+	},
+	errorCallback = function(d) {
+		console.error('Error: '+d);       			
+	};
+
+	$scope.loginSubmit = function(){
+		if( $scope.inputId && $scope.inputPassword ) {
+			config.params.user_id = $scope.inputId;
+			config.params.pwd = $scope.inputPassword;
+
+			$http(config).then(successCallback,errorCallback)
+		}
+	};
 }]);
